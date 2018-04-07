fb808fdb926aa33a9c098a867238795d25c3b564
Christian <krizmarles@gmail.com>
2018-04-06 21:36:51 -0500

    NodeJS Tuto

---
+++ app.js
@@ -0,0 +1,16 @@
+const os = require('os');
+const fs = require('fs');
+
+let cpu = os.cpus();
+let system = os.platform();
+let user = os.hostname();
+
+//console.log(cpu,system);
+//console.log(user);
+let cpu_string = JSON.stringify(cpu);
+
+fs.appendFile('file.txt',`CPU Info: ${cpu_string}`  , function(error){
+  if (error) {
+    console.log("Error creating the file");
+  }    
+});
